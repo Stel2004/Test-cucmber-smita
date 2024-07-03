38fa723e112e90bead8d3e5404a16728c4fd2f59
shitalsanghani <sanghanishital00@gmail.com>
2024-07-03 12:05:20 +0100

    done

--- src/main/java/com/bank/Test.java
+++ src/main/java/com/bank/Test.java
@@ -5,5 +5,6 @@ public class Test {
     public void test(){
         System.out.println("Hello from smita");
         System.out.println("Hello from World");
+        System.out.println("Hello everyone");
     }
 }
