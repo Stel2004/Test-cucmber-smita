a89c2c2c4bc7caba241896dd73cbfb628b28ea3d
svaja <smitavaja.uk@gmail.com>
2024-07-03 11:58:48 +0100

    Done

--- src/main/java/com/bank/Test.java
+++ src/main/java/com/bank/Test.java
@@ -5,4 +5,8 @@ public class Test {
     public void test(){
         System.out.println("Hello from smita");
     }
+
+    public void test1(){
+        System.out.println("Hello from smita");
+    }
 }
